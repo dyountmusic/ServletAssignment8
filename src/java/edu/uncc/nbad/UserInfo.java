0a7b8949e306effe35e4181d8cc99e5cd466f07b
Daniel <yountdaniel@gmail.com>
2018-02-05 16:16:57 -0500

    Javabean class implemented with getters and setters generated.

---
+++ src/java/edu/uncc/nbad/UserInfo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uncc.nbad;
+import java.io.Serializable;
+
+/**
+ *
+ * @author danielyount
+ */
+public class UserInfo implements Serializable {
+    
+    private String name = "";
+    private String username = "";
+    private String passid = "";
+    private String address = "";
+    private String country = "";
+    private String zip = "";
+    private String email = "";
+    private String sex = "";
+    private String langs = "";
+    private String about = "";
+    
+    public UserInfo() {}
+
+}
